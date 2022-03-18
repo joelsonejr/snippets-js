0294100a449bdc9c50b3bf719764dec0b826dfbe
Joelson Junior <53396470+joelsonejr@users.noreply.github.com>
2022-03-18 16:30:25 -0300

    Update ageVerification.js

--- ageVerification.js
+++ ageVerification.js
@@ -1,10 +1,12 @@
 let minimumAge = 18;
+let order;
 
 function ageVerification(){
     let buyersAge = prompt('Qual a sua idade? ');
     
     if(buyersAge >= minimumAge){
         alert('Venda liberada');
+        order = prompt('Qual é o seu pedido? ');
     }
     else{
         alert('A venda é proibida para menores de ' + minimumAge + ' anos.');
