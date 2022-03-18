0fe091dbc3b738f6d798d80f0bbe7d4611ae93e4
Joelson Junior <53396470+joelsonejr@users.noreply.github.com>
2022-03-18 15:41:48 -0300

    Initial commit

---
+++ registerUserName.js
@@ -0,0 +1,9 @@
+let userName = [];
+
+function registerUserName() {
+   let userInput; 
+  
+   userInput = prompt('Please, type in your name '); // may be replaced by document.Get-something.
+   
+   userName.push(userInput);
+}
